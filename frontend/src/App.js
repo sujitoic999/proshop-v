d272d485d3385d6be7d61c37f4d81adde77132ac
sujitoic999 <sujitfgiet@gmail.com>
2025-06-19 23:36:27 +0530

    completed till video 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
+import React from 'react';
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
-    </div>
+    </React.Fragment>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        id: 1,
+        path: '/',
+        element: <HomeScreen />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
